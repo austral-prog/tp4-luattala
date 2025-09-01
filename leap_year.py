4402118ed94340a61244b8ab9e43f86dcf0abe4d
luattala <lattala@mail.austral.edu.ar>
2025-09-01 11:40:57 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,16 @@
 def leap_year():
-    print("TO DO")
+    text = int(input("Ingrese un año: "))
+    resto = text%4
+    cent = text%100
+    cuat = text%400
+    if resto==0:
+        if cent == 0:
+            if cuat == 0:  
+                print(f"El año {text} es bisiesto")
+            else:
+                print(f"El año {texto} no es bisiesto")
+        else:
+            print(f"El año {text} es bisiesto")
+    else:
+        print(f"El año {text} no es bisiesto")
+leap_year()
